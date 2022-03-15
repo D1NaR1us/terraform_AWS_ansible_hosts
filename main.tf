261ead25b914fa536997d940fecc02a5ce6f0c0a
Dinar Gilmutdinov <Dinar_1994@live.com>
2022-03-15 23:49:52 +0400

    asdasd

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ resource "aws_instance" "terraform_ansible_host" {
   provisioner "remote-exec" {
     inline = [
       "sudo apt-get update -y",
-      "sudo apt install firewalld -y",
+      "sudo apt install firewalld",
       "sudo adduser --disabled-password --gecos '' myuser",
       "sudo mkdir -p /home/myuser/.ssh",
       "sudo touch /home/myuser/.ssh/authorized_keys",
@@ -26,7 +26,7 @@ resource "aws_instance" "terraform_ansible_host" {
       "sudo chmod 700 /home/myuser/.ssh",
       "sudo chmod 600 /home/myuser/.ssh/authorized_keys",
       "sudo usermod -aG sudo myuser",
-      "sudo echo 'myuser ALL=(ALL) NOPASSWD: ALL' > touch /etc/sudoers.d/myuser"
+      "sudo sh -c 'echo \"myuser ALL=(ALL) NOPASSWD: ALL\" > etc/sudoers.d/myuser'"
     ]
 
     connection {
