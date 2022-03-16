80bef94c58e9f2251ca315b15c5a1f4eaa5a2e9f
Dinar Gilmutdinov <Dinar_1994@live.com>
2022-03-16 11:44:21 +0400

    zdfgsfd

--- main.tf
+++ main.tf
@@ -16,6 +16,7 @@ resource "aws_instance" "terraform_ansible_host" {
   provisioner "remote-exec" {
     inline = [
       "sudo apt-get update -y",
+      "sudo apt install firewalld -y",
       "sudo adduser --disabled-password --gecos '' myuser",
       "sudo mkdir -p /home/myuser/.ssh",
       "sudo touch /home/myuser/.ssh/authorized_keys",
