6ac017f794d25ff5e772d92eb612f9940fa3442d
Dinar Gilmutdinov <Dinar_1994@live.com>
2022-03-16 10:51:16 +0400

    erhgrtg

--- main.tf
+++ main.tf
@@ -15,9 +15,8 @@ resource "aws_instance" "terraform_ansible_host" {
 
   provisioner "remote-exec" {
     inline = [
-      "sudo apt-get update -y",
+      "sudo apt-get update",
       "sudo apt-get upgrade -y",
-      "sudo apt install firewalld",
       "sudo adduser --disabled-password --gecos '' myuser",
       "sudo mkdir -p /home/myuser/.ssh",
       "sudo touch /home/myuser/.ssh/authorized_keys",
@@ -27,7 +26,8 @@ resource "aws_instance" "terraform_ansible_host" {
       "sudo chmod 700 /home/myuser/.ssh",
       "sudo chmod 600 /home/myuser/.ssh/authorized_keys",
       "sudo usermod -aG sudo myuser",
-      "echo 'myuser ALL=(ALL) NOPASSWD: ALL' | sudo tee -a /etc/conf.d/myuser > /dev/null"
+      "sudo touch /etc/conf.d/myuser",
+      "echo 'myuser ALL=(ALL) NOPASSWD: ALL' | sudo tee -a /etc/sudoers.d/myuser > /dev/null"
     ]
 
     connection {
